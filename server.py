39484293efeb128141319fb6e1cea537122e653a
Eric Carmichael <eric@ckcollab.com>
2016-03-02 15:40:51 -0800

    verify contents of message against our secret

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import json
 import heroku3
+import hmac
+import logging
 import os
 
-from bottle import get, run, post, request, HTTPError, response
-from requests import Session
-from requests import post as request_post
+from bottle import get, run, post, request, HTTPError
+from hashlib import sha1
 
 
 # ----------------------------------------------------------------------------
@@ -13,7 +14,7 @@ from requests import post as request_post
 env_var_names = (
     'HEROKU_API_KEY',
     'HEROKU_BASE_APP_NAME'
-    #'SPECIAL_SECRET',
+    'SPECIAL_SECRET',
 )
 env = {}
 for name in env_var_names:
@@ -35,6 +36,16 @@ def set_heroku_config(application_name, key, value):
 # ----------------------------------------------------------------------------
 @post('/pr_created')
 def pr_created():
+    # We need to verify the message we received against our secret
+    payload = request.body.read()
+    # Example received signature: "sha1=51fb5125e4e65aa893911c89de283576d9c821b5"
+    received_sig = request.headers['X-Hub-Signature'].split('=', 1)[1]
+    computed_sig = hmac.new(env["SPECIAL_SECRET"], payload, sha1).hexdigest()
+    if received_sig != computed_sig:
+        logging.error('Received signature %r does not match' % received_sig)
+        raise HTTPError(403, 'Signature mismatch')
+
+    # The message has been verified, so let's process it!
     pr_number = request.json["pull_request"]["number"]
     new_app_name = "{}-pr-{}".format(env['HEROKU_BASE_APP_NAME'], pr_number)
     set_heroku_config(new_app_name, "HEROKU_APP_NAME", new_app_name)
